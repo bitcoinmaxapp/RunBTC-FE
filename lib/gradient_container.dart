405b8651c9ed31219c740d26ba820b30c38d4e17
efecini <efecini@gmail.com>
2025-02-18 22:17:53 +0300

    added

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:runbtc/styled_text.dart';
 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [
-        Color.fromARGB(255, 35, 7, 84),
-        Color.fromARGB(255, 189, 175, 213)
-      ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-      child: const Center(child: StyledText()),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: const [
+        Color.fromARGB(255, 229, 227, 129),
+        Color.fromARGB(255, 247, 246, 224),
+      ], begin: startAlignment, end: endAlignment)),
+      child: Center(child: Image.asset('assets/images/welcome2.jpg')),
     );
   }
 }
--- lib/styled_text.dart
+++ lib/styled_text.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText({super.key});
+  const StyledText(this.text, {super.key});
+
+  final String text;
 
   @override
   Widget build(context) {
-    return const Text(
-      'Bitcoinmax',
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: 28,
-      ),
+    return Text(
+      text,
+      style: const TextStyle(
+          color: Color.fromARGB(255, 0, 0, 0),
+          fontSize: 50,
+          fontWeight: FontWeight.bold),
     );
   }
 }
